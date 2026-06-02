a4fe6730dce695e52c06f0f103025cba3a56b919
Jonathan Hernandez <fernandomedina7624@gmail.com>
2026-06-01 12:11:16 -1200

    Agregue pantalla About Us

--- src/screens/AboutUsScreen.tsx
+++ src/screens/AboutUsScreen.tsx
@@ -1,5 +1,28 @@
 export const AboutUsScreen = () => {
-    return (
-        <p>AboutUsScreen</p>
-    )
+  return (
+    <section className="about">
+      <div className="about-container">
+        <h1>Sobre Nosotros</h1>
+
+        <p>
+          En Las delicias de la Birria nos especializamos en ofrecer la mejor birria
+          con recetas tradicionales, ingredientes frescos y un sabor
+          auténtico que representa la cocina mexicana.
+        </p>
+
+        <p>
+          Nuestro objetivo es brindar una experiencia única a cada
+          cliente, ofreciendo tacos, quesabirrias, consomé y bebidas
+          preparados con calidad y pasión.
+        </p>
+
+        <p>
+          Gracias por confiar en nosotros y formar parte de nuestra
+          comunidad amante de la birria.
+        </p>
+
+        <button>Ver Menú</button>
+      </div>
+    </section>
+  )
 }
